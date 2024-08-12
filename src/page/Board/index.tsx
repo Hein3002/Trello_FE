896a21c305e07155b8deee2e001af03f556aab90
HipLaAi <gvenh59@gmail.com>
2024-08-13 00:44:31 +0700

    update

--- src/page/Board/index.tsx
+++ src/page/Board/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const index = () => {
+const Board = () => {
   return (
     <div>
       Home
@@ -8,4 +8,4 @@ const index = () => {
   )
 }
 
-export default index
+export default Board
